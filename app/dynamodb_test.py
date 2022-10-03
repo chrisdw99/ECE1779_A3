abb6e623fc0f349944f032ebd0b5108ce6ad54cc
wangyilong <wangyilongcs@outlook.com>
2022-10-03 03:58:58 -0400

    change test info

--- app/dynamodb_test.py
+++ app/dynamodb_test.py
@@ -1,32 +1,6 @@
 #import boto3
 from dynamodb_operations import update_resident,delete_resident,get_all_residents,get_resident_doses,get_image_status,set_image_status_code
-#from operations import get_image_number
-# def create_residents_table(dynamodb=None):
-#     if not dynamodb:
-#         #This is just for local test use
-#         dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
 
-#     table = dynamodb.create_table(
-#         TableName='Residents',
-#         KeySchema=[
-#             {
-#                 'AttributeName': 'residentid',
-#                 'KeyType': 'HASH'  # Partition key
-#             }
-#         ],
-#         AttributeDefinitions=[
-#             {
-#                 'AttributeName': 'residentid',
-#                 'AttributeType': 'N'
-#             }
-
-#         ],
-#         ProvisionedThroughput={
-#             'ReadCapacityUnits': 50,
-#             'WriteCapacityUnits': 10
-#         }
-#     )
-#     return table
 status_code_dic = {"0":"1","1":"2","2":"1","3":"3","4":"1","5":"2","6":"1"}
 status_code_add = {"0":"1","1":"3","2":"3","3":"7","4":"5","5":"7","6":"7"}
 status_code_del=   {("1","1"):"0",("1","3"):"2",("1","5"):"4",("1","7"):"6",
